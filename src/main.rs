cc79a038ece095f77c915101d0144829c4c52885
Atsutomo Tabata <atabata@yahoo-corp.jp>
2022-12-31 22:14:14 +0900

    add DummyAllocator

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@
 #![test_runner(my_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
+use alloc::boxed::Box;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use my_os::memory::BootInfoFrameAllocator;
@@ -23,15 +26,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // flame allocator作成
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    // 未使用のページをマップする
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    // 上記のマッピングを使って画面表示する
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe {
-        page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e);
-    }
+    let x = Box::new(41);
 
     #[cfg(test)]
     test_main();
