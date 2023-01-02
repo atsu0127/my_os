aac3309f7ac73734f8d14188865ec2e7b01a22d5
Atsutomo Tabata <atabata@yahoo-corp.jp>
2023-01-02 11:50:41 +0900

    add test for allocator

---
+++ tests/heap_allocation.rs
@@ -0,0 +1,65 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(my_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+extern crate alloc;
+
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+use bootloader::{entry_point, BootInfo};
+use core::panic::PanicInfo;
+use my_os::allocator::HEAP_SIZE;
+use my_os::memory::BootInfoFrameAllocator;
+use my_os::{allocator, memory};
+
+entry_point!(main);
+
+fn main(boot_info: &'static BootInfo) -> ! {
+    use x86_64::VirtAddr;
+
+    my_os::init();
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    test_main();
+    loop {}
+}
+
+// 以下test case
+#[test_case]
+// allocationでエラーが起きないか
+fn simple_allocation() {
+    let heap_value_1 = Box::new(41);
+    let heap_value_2 = Box::new(13);
+    assert_eq!(*heap_value_1, 41);
+    assert_eq!(*heap_value_2, 13);
+}
+
+#[test_case]
+// 大きなvecを割り当てられるか
+fn large_vec() {
+    let n = 1000;
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(i);
+    }
+    assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
+}
+
+#[test_case]
+// 複数回アロケーションする
+fn many_boxes() {
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    my_os::test_panic_handler(info)
+}
